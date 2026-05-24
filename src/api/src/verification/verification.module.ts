a3725cc648d5fc08ceba72757076a36c29726c74
Yelyzaveta Rozsokhach <elizabeth.rozsokhach@gmail.com>
2026-05-25 00:55:18 +0300

    Move secrets to environment variables

--- src/api/src/verification/verification.module.ts
+++ src/api/src/verification/verification.module.ts
@@ -3,16 +3,20 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { VerificationTicket } from './entities/verification-ticket.entity';
 import { VerificationService } from './verification.service';
 import { TwilioModule } from 'nestjs-twilio';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TwilioModule.forRoot({
-      accountSid: 'AC9870142d7a30701fbb4cfab16ac5f7fd',
-      authToken: '5da80a513495dd86bb1bd25db314c259',
+    TwilioModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        accountSid: configService.getOrThrow<string>('TWILIO_ACCOUNT_SID'),
+        authToken: configService.getOrThrow<string>('TWILIO_AUTH_TOKEN')
+      }),
     }),
     TypeOrmModule.forFeature([VerificationTicket]),
   ],
   providers: [VerificationService],
   exports: [VerificationService],
 })
-export class VerificationModule {}
+export class VerificationModule { }
